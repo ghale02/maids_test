bc214bafb2e08673a58dc5cee4ee165bdb1cd431
Ghale Abduallah <ghale001.wrok@gmail.com>
2024-05-20 06:55:15 +0300

    todos list cubit di

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -12,6 +12,14 @@ import 'package:maids_test/features/login/domain/use_cases/login_usecase.dart';
 import 'package:maids_test/features/login/presentation/blocs/auto_login_cubit.dart';
 import 'package:maids_test/features/login/presentation/blocs/login_cubit.dart';
 import 'package:http/http.dart' as http;
+import 'package:maids_test/features/todos/data/providers/todos_cache_provider_abstract.dart';
+import 'package:maids_test/features/todos/data/providers/todos_cache_provider_local.dart';
+import 'package:maids_test/features/todos/data/providers/todos_provider_abstract.dart';
+import 'package:maids_test/features/todos/data/providers/todos_provider_api.dart';
+import 'package:maids_test/features/todos/data/repositories/todo_repository_impl.dart';
+import 'package:maids_test/features/todos/domain/repositories/todos_repository_abstract.dart';
+import 'package:maids_test/features/todos/domain/use_cases/get_todos_usecase.dart';
+import 'package:maids_test/features/todos/presentation/blocs/todos_list_cubit.dart';
 import 'package:maids_test/shared/providers/connection_checker_abstract.dart';
 import 'package:maids_test/shared/providers/connection_checker_impl.dart';
 
@@ -19,7 +27,9 @@ final sl = GetIt.instance;
 
 void init() {
   _login();
+  _todos();
   _externals();
+  _shared();
 }
 
 void _login() {
@@ -36,12 +46,15 @@ void _login() {
   sl.registerLazySingleton<AuthRepositoryAbstract>(() => AuthRepositoryApi(
       authProvider: sl(), authManager: sl(), connectionChecker: sl()));
 
-  // register auth manager
-  sl.registerLazySingleton<AuthManagerAbstract>(
-      () => AuthManagerLocal(storage: sl()));
   //register auth provider
   sl.registerLazySingleton<AuthProviderAbstract>(
       () => AuthProviderApi(client: sl()));
+}
+
+void _shared() {
+  // register auth manager
+  sl.registerLazySingleton<AuthManagerAbstract>(
+      () => AuthManagerLocal(storage: sl()));
   //register connection checker
   sl.registerLazySingleton<ConnectionCheckerAbstract>(
       () => ConnectionCheckerImpl(sl()));
@@ -53,4 +66,25 @@ void _externals() {
   //register flutter secure  storage
   sl.registerLazySingleton(() => const FlutterSecureStorage());
   sl.registerLazySingleton(() => InternetConnectionChecker());
+}
+
+void _todos() {
+  // register get todos cubit
+  sl.registerFactory(() => TodosListCubit(sl()));
+  //register get todos usecase as lazy singleton
+  sl.registerLazySingleton(() => GetTodosUseCase(repository: sl()));
+
+  // register todos repository
+  sl.registerLazySingleton<TodosRepositoryAbstract>(() => TodoRepositoryImpl(
+        apiProvider: sl(),
+        authManager: sl(),
+        cache: sl(),
+        connectionChecker: sl(),
+      ));
+  //register todos api provider
+  sl.registerLazySingleton<TodosProviderAbstract>(
+      () => TodosProviderApi(client: sl()));
+  //register todos cache provider
+  sl.registerLazySingleton<TodosCacheProviderAbstract>(
+      () => TodosCacheProviderLocal(database: sl()));
 }
